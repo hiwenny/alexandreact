cb998a16854b143407d7697045de8a06917e2e73
Wenny Hidayat <wenny.h@live.com>
2017-06-19 01:31:03 +1000

    Sass config, not working yet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+require('./styles/app.scss');
 
 class App extends Component {
   render() {
