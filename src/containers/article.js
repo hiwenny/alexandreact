fbba9029a36078ee1ccd100a1f8b140963b8c134
Wenny Hidayat <wenny.h@live.com>
2017-06-24 13:44:08 +1000

    Added more dummy texts

--- src/containers/article.js
+++ src/containers/article.js
@@ -4,10 +4,11 @@ require('../styles/article.css');
 export class Article extends Component {
   render() {
     console.log(this.props);
+    let { content } = this.props;
     return (
       <div className="article-container">
-        <h1>{this.props.content.title}</h1>
-        <p>{this.props.content.contents}</p>
+        <h1>{content[1].title}</h1>
+        <p>{content[1].content}</p>
         <div></div>
       </div>
     );
