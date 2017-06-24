e84759f852d6e15526a7d780a72831de61e3848d
Wenny Hidayat <wenny.h@live.com>
2017-06-24 13:32:32 +1000

    Cleanup

--- src/containers/article.js
+++ src/containers/article.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 require('../styles/article.css');
 
 export class Article extends Component {
-  // crapDataPreprocessing(text) {
-  //   text.replace('<p>')
-  // }
-
   render() {
     console.log(this.props);
     return (
       <div className="article-container">
         <h1>{this.props.content.title}</h1>
-        <h3>While this is a subtitle</h3>
         <p>{this.props.content.contents}</p>
-        <p style={{whiteSpace:"pre-wrap"}}>{"Hi\n ya"}</p>
         <div></div>
       </div>
     );
