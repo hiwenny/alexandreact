1a0e9e6393a91fb40cabdddaee4adaa95acfdb4c
Wenny Hidayat <wenny.h@live.com>
2017-06-24 13:32:00 +1000

    Added dummy text to test content rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Article } from './containers/index';
 require('./styles/main.css');
+var tempdata = require('./temp/data.json');
 
 export default class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <Article />
+        <Article content={tempdata}/>
       </div>
     );
   }
--- src/containers/article.js
+++ src/containers/article.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 require('../styles/article.css');
 
 export class Article extends Component {
+  // crapDataPreprocessing(text) {
+  //   text.replace('<p>')
+  // }
+
   render() {
+    console.log(this.props);
     return (
       <div className="article-container">
-        <h1>Title</h1>
+        <h1>{this.props.content.title}</h1>
         <h3>While this is a subtitle</h3>
-        <p>This is a p</p>
-        <div>dsadadsada</div>
+        <p>{this.props.content.contents}</p>
+        <p style={{whiteSpace:"pre-wrap"}}>{"Hi\n ya"}</p>
+        <div></div>
       </div>
     );
   }
